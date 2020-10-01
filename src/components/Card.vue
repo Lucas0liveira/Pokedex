842d47786ab8c5dcb16ca5ccf3d61b4b8af574e2
Leozaru <leoufms@gmail.com>
2020-10-01 18:44:09 -0400

    CSS ajustado

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,59 @@
 <template>
   <div class="card">
-    <p>#1</p>
     <div class="pokemon">
-      <img alt="Bulbassauro" src="../assets/bulba.png"  style="height: 120px;"/> 
+      <p>#1</p>
+      <img class="imgPoke" alt="Bulbassauro" src="../assets/bulba.png"  style="height: 120px;"/> 
       </div>
-    <p> Nome do pokemon</p>
-    <p> Tipo 01 </p> 
-    <p> Tipo 02 </p>
-  </div>  
+
+      <p class="nomePoke"> Nome do pokemon</p>
+    
+    <div class="tipos">
+      <p class="tipo"> Tipo 01 </p> 
+      <p class="tipo"> Tipo 02 </p>
+  
+    </div>
+    </div>  
 </template>
 
 <script>
 </script>
 
 <style>
+
+body{
+}
 .card{
+  display:flex;
+  flex-direction: column;
   width:196px;
   height:280px;
-  border:solid 3px #141414;
+  border:solid 2px #666666;
   border-radius: 5px;
   background-color: #FFFFFF;
-
+}
+.pokemon{
+  background-color:greenyellow;
+  height: 100;
+  border-radius: 5px; 
+  border-bottom: solid 1px #141414;
+}
+.imgPoke{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.nomePoke{
+  text-align: center;
+  font-size: 20px;
+}
+.tipo{  
+  background-color: #f1f1f1;
+  margin: 2px;
+  padding: 5px;
+  font-size: 15px;
+}
+.tipos{
+  display: flex;
+  justify-content: center;
 }
 </style>
